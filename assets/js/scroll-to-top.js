7ac26ea4045d636868a1119292e7e45d0564e402
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-03-10 10:04:14 +0100

    fixed debug warning in Scroll to Top script

--- assets/js/scroll-to-top.js
+++ assets/js/scroll-to-top.js
@@ -46,7 +46,7 @@
 	$( document ).ready( function() {
 
 		/* Add Button to HTML DOM */
-		$( 'body' ).append( '<button id=\"scroll-to-top\" class=\"scroll-to-top-button\">' + occasio_pro_scroll_button + '</button>' );
+		$( 'body' ).append( '<button id=\"scroll-to-top\" class=\"scroll-to-top-button\">' + occasioProScrollToTop.icon + '</button>' );
 
 		/* Add Scroll To Top Functionality */
 		$( '#scroll-to-top' ).scrollToTop();
